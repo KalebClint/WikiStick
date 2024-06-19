7c1b14b2cafbc75fb7d9d73667309f0719a2d1fa
KalebClint <kalebclintmail@gmail.com>
2024-06-19 16:32:39 +1200

    more

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     // List of pages (URLs) to select from
     const pages = [
-        { url: 'https://www.example1.com', name: 'Example 1' },
-        { url: 'https://www.example2.com', name: 'Example 2' },
-        { url: 'https://www.example3.com', name: 'Example 3' },
-        { url: 'https://www.example4.com', name: 'Example 4' },
-        { url: 'https://www.example5.com', name: 'Example 5' },
-        { url: 'https://www.example6.com', name: 'Example 6' }
+        { url: 'GameDevelopement/amuletOfKatari.html', name: 'Amulet Of Katari' },
+        { url: 'GameDevelopement/deeperDepths.html', name: 'Deeper Depths' },
+        { url: 'GameDevelopement/humblingRequest.html', name: 'A Humbling Request' },
+        { url: 'GameDevelopement/RP3DG.html', name: 'Random Procedual 3D Map Generation' },
+        { url: 'GameDevelopement/statues.html', name: 'Statues' },
+        { url: 'GameDevelopement/statuesRemake.html', name: 'Statues Remake' },
+        { url: "GameDevelopement/vampireVille.html", name: "A Sunny Day At Vampireville" }
+
     ];
 
     // Function to get a random page
